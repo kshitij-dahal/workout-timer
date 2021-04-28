f08c27ee98086bb245d2d3d6b3c3ba60093e604a
Kshitij Dahal <kshitijdahal314@gmail.com>
2021-04-28 17:45:08 -0400

    Allow user to edit name, rep and sets information of workout

--- workoutTimer/components/Routine.js
+++ workoutTimer/components/Routine.js
@@ -5,7 +5,7 @@ import Workout from './Workout';
 const Routine = ({data}) => {
   return (
     <List.Accordion
-      title="Uncontrolled Accordion"
+      title={data.routine.name}
       left={props => <List.Icon {...props} icon="folder" />}>
       {data.routine.workouts.map(workout => (
         <Workout data={workout} />
--- workoutTimer/components/Workout.js
+++ workoutTimer/components/Workout.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import {List} from 'react-native-paper';
-import {TextInput} from 'react-native';
+import {TextInput, Text, View} from 'react-native';
 
 const Workout = ({data}) => {
   const [workoutInfo, setWorkoutInfo] = React.useState(data);
 
   return (
     <List.Accordion
-      title={data.name}
+      title={workoutInfo.name}
       style={{width: '95%', left: '5%'}}
       left={props => <List.Icon {...props} icon="folder" />}>
-      {Object.keys(data).map(key => (
-        <TextInput
-          onChangeText={text => {
-            let workoutInfoCopy = JSON.parse(JSON.stringify(workoutInfo));
-            workoutInfoCopy[key] = text;
-            setWorkoutInfo(workoutInfoCopy);
-          }}
-          value={data[key]}
-          placeholder={String(data[key])}
-          keyboardType="numeric"
-        />
-      ))}
+      {Object.keys(workoutInfo).map(key => {
+        return (
+          <View
+            style={{
+              flexDirection: 'row',
+            }}>
+            <Text style={{flex: 1}}>{key}</Text>
+            <TextInput
+              onChangeText={text => {
+                let workoutInfoCopy = JSON.parse(JSON.stringify(workoutInfo));
+                workoutInfoCopy[key] = text;
+                setWorkoutInfo(workoutInfoCopy);
+              }}
+              style={{flex: 5}}
+              value={workoutInfo[key]}
+              placeholder={String(workoutInfo[key])}
+              keyboardType="numeric"
+            />
+          </View>
+        );
+      })}
     </List.Accordion>
   );
 };
